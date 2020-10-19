bcddaac7df6e669ce9fe7e263fed77167f581823
Jan-Hendrik van Heusden <janhvh@yahoo.com>
2020-10-19 21:30:01 +0200

    Added Apache 2.0 License

--- src/main/kotlin/nl/jhvh/sudoku/format/boxformat/RowBoxFormatter.kt
+++ src/main/kotlin/nl/jhvh/sudoku/format/boxformat/RowBoxFormatter.kt
@@ -1,5 +1,20 @@
 package nl.jhvh.sudoku.format.boxformat
 
+/* Copyright 2020 Jan-Hendrik van Heusden
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import nl.jhvh.sudoku.format.Formattable.FormattableList
 import nl.jhvh.sudoku.format.element.RowFormatting
 import nl.jhvh.sudoku.grid.model.segment.Row
